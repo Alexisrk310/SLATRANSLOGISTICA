2f456eec6ed073a00e6faf522618589c301bbc94
Alexis Gutierrez <alexisrk310@gmail.com>
2025-08-08 15:43:50 -0500

    mejorando el diseño de nofoundpage

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,13 +5,11 @@ import { FaArrowLeft } from 'react-icons/fa';
 export default function NotFound() {
 	return (
 		<div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-sky-400 via-orange-50 to-sky-400 px-6 text-center">
-			<h1 className="text-9xl font-extrabold text-sky-600 drop-shadow-[0_3px_6px_rgba(0,0,0,0.6)]">
-				404
-			</h1>
-			<h2 className="text-2xl md:text-3xl font-bold mt-4 text-sky-600 drop-shadow-[0_2px_4px_rgba(0,0,0,0.6)]">
+			<h1 className="text-9xl font-extrabold text-sky-600 ">404</h1>
+			<h2 className="text-2xl md:text-3xl font-bold mt-4 text-sky-600 ">
 				Página no encontrada
 			</h2>
-			<p className="mt-2 max-w-md text-sky-600 drop-shadow-[0_2px_3px_rgba(0,0,0,0.6)]">
+			<p className="mt-2 max-w-md text-sky-600 ">
 				La página que buscas no existe o fue movida. Verifica la URL o vuelve a
 				la página principal.
 			</p>
